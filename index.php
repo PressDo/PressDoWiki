4b94ebb33f5c14ac43564594f59744928cef87e7
PRASEOD- <aaei924@gmail.com>
2021-01-21 17:39:05 +0900

    Update index.php
    기능 분리

--- index.php
+++ index.php
@@ -1,30 +1,41 @@
 <?php
 use PressDo\PressDo;
 use PressDo\WikiSkin;
-use PressDo\SQL;
+use PressDo\Data;
 include 'config.php';
 require_once 'PressDoLib.php';
 require_once './skin/'.$conf['Skin'].'/skin.php';
 
-if(!$_GET['title']){
+if(isset($_POST['title'])){
+    $Title = $_POST['title'];
+}elseif(!$_GET['title']){
     $Title = $conf['Title'];
 }else{
     $Title = $_GET['title'];
 }
 
-if($_GET['action'] == 'save'){
-    // 편집 저장
-}elseif($_GET['action'] == 'edit'){
-    // 편집 모드
-}elseif($_GET['action'] == 'move'){
-    // 문서이동 모드
-}elseif($_GET['action'] == 'delete'){
-    // 삭제 모드
-}elseif($_GET['action'] == 'acl'){
-    // ACL 모드
-}elseif($_GET['action'] == 'view' || !$_GET['action']){
-    // 보기 모드
-WikiSkin::FullPage($Title, PressDo::readSyntax(SQL::LoadDocument($_GET['title'])));
+if($_POST['action'] == 'save'){
+    $act = 'save';
 }
+
+// 제목 조작 방지
+function getTitle(){
+    parse_str(parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY), $i);
+    if(!$i['title']){
+        return false;
+    }else{
+        return $i['title'];
+    }
+}
+if($act == 'save'){
+    // 편집 저장
+    if(!getTitle() || getTitle() !== $Title){
+        echo '잘못된 시도입니다.';
+        exit;
+    }
+    Data::SaveDocument(getTitle(), $_POST['content'], $_POST['summary']);
+}
+    $Doc_content = Data::LoadLatestDocument($Title);
+    WikiSkin::FullPage($Title, $Doc_content[1], 'view');
 
 ?>
