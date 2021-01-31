33ddbe8d237224bdda7178402dfa824ec990f50a
PRASEOD- <aaei924@gmail.com>
2021-01-31 14:03:03 +0900

    Update syntax.php

--- syntax.php
+++ syntax.php
@@ -6,9 +6,10 @@ PressDo Wiki Syntax Processor
 */
 function readSyntax($content)
 {
+
     /* NamuMark PHP Library by koreapyj */
     // 라이브러리를 불러옵니다.
-    require_once("./NamuMark.php");
+    require_once("NamuMark.php");
 
     // MySQLWikiPage와는 달리 PlainWikiPage의 첫 번째 인수로 위키텍스트를 받습니다.
     $wPage = new PlainWikiPage($content);
@@ -17,10 +18,13 @@ function readSyntax($content)
     $wEngine = new NamuMark($wPage);
 
     // 위키링크의 앞에 붙을 경로를 prefix에 넣습니다.
-    $wEngine->prefix = $conf['Domain'].'index.php?title=';
+    $wEngine->prefix = $conf['Domain'].'/w/';
 
     // toHtml을 호출하면 HTML 페이지가 생성됩니다.
-    $content = $wEngine->toHtml();
+    $content = $wEngine->toHtml(); 
+
+    // 리스트
+    
 
     // 각주 크기
     $content = preg_replace('/<a id=\"rfn-(.*?)<\/a>/s', '<sup>$0</sup>', $content);
@@ -28,8 +32,6 @@ function readSyntax($content)
     // 볼드체
     $content = preg_replace('/(?<!{{{)\*\*(.*?)\*\*/s', '<b>$1</b>', $content);
     
-    // 이탤릭(반드시 볼드체보다 나중에)
-    $content = preg_replace('/(?<!{{{)\*(.*?)\*/s', '<i>$1</i>', $content);
 
     // 주석
     $content = preg_replace('/^\/\/(.*?)$/m', '', $content);
