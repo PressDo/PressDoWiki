a002e5b50d2ed0517ba74e4d9f8072201604e00c
PRASEOD- <aaei924@gmail.com>
2021-02-14 16:51:02 +0900

    Create config.php

---
+++ data/global/config.php
@@ -0,0 +1,41 @@
+<?php if(!defined('PressDo_Config')) exit;
+$conf = array(
+    "Name" => "PressDo 테스트 위키",
+    "NameSpace" => "PressDoWiki",
+    "Domain" => "",
+    "ScriptPath" => "/",
+    "ViewerUri" => "/w/",
+    "Language" => "ko-kr",
+    "Title" => "대문",
+    "CopyRight" => "PRASEOD-",
+    "HelpMail" => "<a href=\"\">Contact Us</a>",
+    "TermsOfUse" => "",
+    "SecPolicy" => "",
+    "DBType" => "",
+    "DBHost" => "",
+    "DBPort" => "",
+    "DBName" => "",
+    "DBUser" => "",
+    "DBPass" => "!",
+    "Uploadable" => true,
+    "AllowFileExt" => ["PNG", "JPG", "JPEG", "GIF", "SVG"],
+    "CompressFile" => true,
+    "PublicLevel" => 0,
+    "AllowJoin" => true,
+    "Skin" => "senkawa",
+    "TitleText" => "PressDoWiki",
+    "LogoWidth" => "6.6rem",
+    "ACLAutoTemplate" => true,
+    "NameSpaceACL" => "",
+    "UseMailWhitelist" => true,
+    "MailWhitelist" => [
+        "gmail.com",
+        "naver.com",
+        "kakao.com",
+        "prws.kr"
+    ],
+    "Extension" => [
+        "TestExtension"
+    ],
+    "comments" => "본 위키는 PressDo 엔진 테스트를 목적으로 생성되었습니다."
+);
