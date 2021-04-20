986f9a500b35af14bcff7cfee7acd50df5b52e77
PRASEOD- <aaei924@gmail.com>
2021-04-20 13:43:05 +0900

    Update syntax.php

--- syntax.php
+++ syntax.php
@@ -7,7 +7,14 @@ function readSyntax($content, $noredirect = 0)
     global $conf;
     require_once('NamuMark.php');
     $NM = new NamuMark(array('read' => $content), $conf['허용사진확장자']);
-    $content = $NM->parse();
+    $content = $NM->parse(function($err, $result) {
+    // result object can be diffrerent by renderer.
+    if ($err)
+        //console.log('ERROR!');
+    $html = $result['html'];
+    $categories = $result['categories'];
+    //console.log('complete!');
+});
     
     return $content;
 }
