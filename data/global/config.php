fceff96db419a04f8289feb7390a73733f3f67d8
PRASEOD- <aaei924@gmail.com>
2021-04-26 13:04:50 +0900

    Update config.php

--- data/global/config.php
+++ data/global/config.php
@@ -1,16 +1,18 @@
 <?php if(!defined('PressDo_Config')) exit;
 $conf = array(
-    "Name" => "PressDo 테스트 위키",
+    "SiteName" => "PressDo 테스트 위키",
     "NameSpace" => "PressDoWiki",
     "Domain" => "",
     "ScriptPath" => "/",
     "ViewerUri" => "/w/",
     "Language" => "ko-kr",
-    "Title" => "대문",
-    "CopyRight" => "PRASEOD-",
-    "HelpMail" => "<a href=\"\">Contact Us</a>",
-    "TermsOfUse" => "",
-    "SecPolicy" => "",
+    "FrontPage" => "대문",
+    "CopyRightText" => "\u003Cimg alt=\"크리에이티브 커먼즈 라이선스\" style=\"border-width: 0;\" src=\"/img/cc-by-nc-sa-2.0-88x31.png\"\u003E\u003Cbr\u003E\r\n이 저작물은 \u003Ca rel=\"license\" href=\"//creativecommons.org/licenses/by-nc-sa/2.0/kr/\"\u003ECC BY-NC-SA 2.0 KR\u003C/a\u003E에 따라 이용할 수 있습니다.",
+    "EditAgreeText" => "문서 편집을 \u003Cstrong\u003E저장\u003C/strong\u003E하면 당신은 기여한 내용을 \u003Cstrong\u003ECC-BY-NC-SA 2.0 KR\u003C/strong\u003E으로 배포하고 기여한 문서에 대한 하이퍼링크나 URL을 이용하여 저작자 표시를 하는 것으로 충분하다는 데 동의하는 것입니다. 이 \u003Cstrong\u003E동의는 철회할 수 없습니다.\u003C/strong\u003E",
+    "CopyrightURL" => "",
+    "CannonicalURL" => "",
+    "SiteNotice" => "",
+    "LogoURL" => "/src/img/logo.png",
     "DBType" => "",
     "DBHost" => "",
     "DBPort" => "",
@@ -38,4 +40,6 @@ $conf = array(
         "TestExtension"
     ],
     "comments" => "본 위키는 PressDo 엔진 테스트를 목적으로 생성되었습니다."
+    "forceRecaptchaPublic" => "",
+    "recaptchaPublic" => ""
 );
