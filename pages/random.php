f40f63ebf133858638bec21b22f24497a710cca0
PRASEOD- <aaei924@gmail.com>
2021-06-14 01:54:10 +0900

    Delete pages directory

--- pages/random.php
+++
@@ -1,8 +0,0 @@
-<?php
-use PressDo\PressDo;
-use PressDo\WikiSkin;
-use PressDo\Data;
-require_once '../PressDoLib.php';
-
-$r = Data::goRandom();
-Header('Location: http://'.$conf['Domain'].$conf['ViewerUri'].$r['DocNm']);
